85d3d04b7caf75f8c15ef4581cc31f27d324a725
Guilherme Quirino <guilherme_quirino@hotmail.com>
2020-03-21 20:33:09 -0300

    feat: create Game class

---
+++ lib/game.rb
@@ -0,0 +1,36 @@
+class Game
+  attr_accessor :id, :players, :total_kills, :kills, :kills_by_means
+
+  def initialize(id)
+    @id = id
+    @players = []
+    @total_kills = 0
+    @kills = Hash.new { |h, k| h[k] = 0 }
+    @kills_by_means = Hash.new { |h, k| h[k] = 0 }
+  end
+
+  def add_player(player_name)
+    players.push(player_name) unless players.include?(player_name)
+  end
+
+  def add_kill(killer, killed, mean)
+    increase_kill(total_kills)
+    kills_by_means[mean] = increase_kill(kills_by_means[mean])
+
+    if killer == '<world>'
+      kills[killed] = decrease_kill(kills[killed])
+    else
+      kills[killer] = increase_kill(kills[killer])
+    end
+  end
+
+  private
+
+  def increase_kill(kill)
+    kill + 1
+  end
+
+  def decrease_kill(kill)
+    kill - 1
+  end
+end
